8c2b6276d702b2e649b0acc01f191c824694fb1e
margot <margot.bonafos@gmail.com>
2025-10-01 16:16:55 +0200

    surname and name choosen OK

--- creation.py
+++ creation.py
@@ -1,4 +1,5 @@
 from random import randint
+import requests
 
 def choose_random_string(file_name):
     with open(file_name, "r", encoding="utf8") as f:
@@ -10,3 +11,12 @@ def choose_random_string(file_name):
 nameChoosen = choose_random_string("names_dictionnary.txt")
 print(nameChoosen)
 
+surnameChoosen = choose_random_string("surnames_dictionnary.txt")
+print(surnameChoosen)
+
+
+s = requests.Session()
+s.headers.update({"User-Agent":"YourTool/1.0"})
+r = s.get("https://exemple.com/api/items?limit=20")
+r.raise_for_status()
+print(r.json())
